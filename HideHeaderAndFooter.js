83079e2050b307b965da03688d61bb43ed1d6c9c
Jumborin <jumborin@users.noreply.github.com>
2021-09-13 23:52:07 +0900

    チケットパネル操作時に再表示されてしまう不具合を修正
    チケットパネル操作時に再表示されてしまう不具合を修正

--- HideHeaderAndFooter.js
+++ HideHeaderAndFooter.js
@@ -15,14 +15,6 @@ $(function () {
     ticket_panel_header_hide();
     ticket_panel_footer_hide();
     
-    /* ステータス変更時など、DOMが差し替えられる度に実行 */
-    var _replaceDomWith = replaceDomWith;
-    replaceDomWith = function(html){
-        _replaceDomWith(html);
-        ticket_panel_header_hide();
-        ticket_panel_footer_hide();
-    };
-    
     /* Dropイベント後のAjax処理が全部終了(Stop)後に再実行 */
     $(document).ajaxStop(function(){
         ticket_panel_header_hide();
