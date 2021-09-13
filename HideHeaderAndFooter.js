23209ba168b34b8df47d8169f31c4ce6cf83e2b0
Jumborin <jumborin@users.noreply.github.com>
2021-09-13 23:37:50 +0900

    Update HideHeaderAndFooter.js
    初期表示のみでなく、チケットパネルの操作時も処理が実行されるように修正

--- HideHeaderAndFooter.js
+++ HideHeaderAndFooter.js
@@ -1,7 +1,31 @@
+/*
+* チケットパネルのヘッダーとフッターを非表示化する
+*/
 $(function () {
-    // ヘッダーを非表示化
-    $(".header").hide();
-
-    // フッター（ウォッチ）を非表示化
-    $(".footer").hide();
+    var ticket_panel_header_hide = function(){
+        // ヘッダーを非表示化
+        $(".header").hide();
+    };
+    var ticket_panel_footer_hide = function(){
+        // フッター（ウォッチ）を非表示化
+        $("div .footer").hide();
+    };
+    
+    /* 初期表示 */
+    ticket_panel_header_hide();
+    ticket_panel_footer_hide();
+    
+    /* ステータス変更時など、DOMが差し替えられる度に実行 */
+    var _replaceDomWith = replaceDomWith;
+    replaceDomWith = function(html){
+        _replaceDomWith(html);
+        ticket_panel_header_hide();
+        ticket_panel_footer_hide();
+    };
+    
+    /* Dropイベント後のAjax処理が全部終了(Stop)後に再実行 */
+    $(document).ajaxStop(function(){
+        ticket_panel_header_hide();
+        ticket_panel_footer_hide();
+    });
 });
