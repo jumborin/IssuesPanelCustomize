d9e1251cfac42f33e94abf42c185158ec02a4bc9
Jumborin <jumborin@users.noreply.github.com>
2021-11-20 10:21:19 +0900

    色の変更をしやすいようにカラーコードをそれぞれ定数として設定。色の追加をしやすいように配列に設定。

--- ColorOfAssignee.js
+++ ColorOfAssignee.js
@@ -1,28 +1,25 @@
+/**
+ * UserのIDの余りによって、色分けを行う。
+ * 
+ * @author jumborin
+ */
 $(function () {
-    color();
-});
+    var ticket_panel_coloring = function () {
+        // 色設定
+        const GREEN = "#AEFFBD";
+        const BLUE = "#CCFFFF";
+        const PURPLE = "#FFCCFF";
+        const RED = "#FFAD90";
+        const PINK = "#FFABCE";
+        const YELLOW = "#FFFFBF";
+        const COLORING = [GREEN, BLUE, PURPLE, RED, PINK, YELLOW];
 
-function color() {
-    $(".card-content").each(function () {
-        switch ($(this).children(".assigned_to").children(".value").children(".user").attr("href").split("/")[2] % 6) {
-            case 0:
-                $(this).css("backgroundColor", "#AEFFBD");
-                break;
-            case 1:
-                $(this).css("backgroundColor", "#CCFFFF");
-                break;
-            case 2:
-                $(this).css("backgroundColor", "#FFCCFF");
-                break;
-            case 3:
-                $(this).css("backgroundColor", "#FFAD90");
-                break;
-            case 4:
-                $(this).css("backgroundColor", "#FFABCE");
-                break;
-            case 5:
-                $(this).css("backgroundColor", "#D0B0FF");
-                break;
-        }
-    });
-}
+        // 処理
+        $(".card-content").each(function () {
+            $(this).css("backgroundColor", COLORING[$(this).children(".assigned_to").children(".value").children(".user").attr("href").split("/")[2] % 6]);
+        });
+    };
+
+    ticket_panel_coloring();
+
+});
